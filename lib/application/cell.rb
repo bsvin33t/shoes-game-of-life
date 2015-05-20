029f88dc43de3f89dd2b9bd3fcc3f72833efed15
bsvin33t <bsv.1511@gmail.com>
2015-05-20 22:01:19 +0530

    Added an interface on the cell to increment the neighbour count Added a method to check if the cell is going to survive to the next tick

--- lib/application/cell.rb
+++ lib/application/cell.rb
@@ -1,14 +1,27 @@
 class Cell
 
-  attr_reader :location
+  attr_reader :location, :neighbour_count
 
   def initialize(*location)
     @location = location
+    @neighbour_count = 0
   end
 
   def ==(other_cell)
     (object_id == other_cell.object_id) || (hash == other_cell.hash) || (self.class == other_cell.class && location == other_cell.location)
   end
+
   alias :eql? :==
 
+  def alive_for_next_tick?
+    if neighbour_count == 2 || neighbour_count == 3
+      true
+    else
+      false
+    end
+  end
+
+  def increment_neighbour
+    @neighbour_count +=1
+  end
 end
--- spec/unit/application/cell_spec.rb
+++ spec/unit/application/cell_spec.rb
@@ -17,4 +17,42 @@ describe Cell do
       expect(cell).to eq(cell)
     end
   end
+
+  describe 'increment neighbour' do
+    it 'should incriment the number of neighbours to a cell' do
+      cell = Cell.new(5, 10)
+      cell.increment_neighbour
+      expect(cell.neighbour_count).to eq(1)
+    end
+  end
+
+  it 'should die in the next tick if neighbour population is zero' do
+    cell = Cell.new(5, 10)
+    expect(cell.alive_for_next_tick?).to eq(false)
+  end
+
+  it 'should die in the next tick if neighbour population is one' do
+    cell = Cell.new(5, 10)
+    1.times {cell.increment_neighbour}
+    expect(cell.alive_for_next_tick?).to eq(false)
+  end
+
+  it 'should live in the next tick if the neighbour population is two' do
+    cell = Cell.new(5, 10)
+    2.times {cell.increment_neighbour}
+    expect(cell.alive_for_next_tick?).to eq(true)
+  end
+
+  it 'should live in the next tick if the neighbour population is three' do
+    cell = Cell.new(5, 10)
+    3.times {cell.increment_neighbour}
+    expect(cell.alive_for_next_tick?).to eq(true)
+  end
+
+  it 'should die in the next tick if neighbour population is four' do
+    cell = Cell.new(5, 10)
+    4.times {cell.increment_neighbour}
+    expect(cell.alive_for_next_tick?).to eq(false)
+  end
+
 end
